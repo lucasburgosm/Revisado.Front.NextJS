0f09e97909b9e4748cf4ba76c5b0efd06901dc51
Joaquin Montesano <montesano1994@gmail.com>
2023-01-27 17:17:35 -0300

    added cors to login and changed next.config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,27 @@
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   test: /\\.(png|jp(e*)g|svg|gif)$/, 
+//   use: ['file-loader'],
+//   env: {
+//     // API_URL: 'https://revisado-back.onrender.com'
+//     API_URL: 'http://192.168.0.128:3001'
+//   }
+// }
+
+// module.exports = nextConfig
+
+
+/** @type {import('next').NextConfig} **/
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   test: /\\.(png|jp(e*)g|svg|gif)$/, 
   use: ['file-loader'],
   env: {
-    API_URL: 'https://revisado-back.onrender.com'
+    // API_URL: 'https://revisado-back.onrender.com'
+    API_URL: 'http://localhost:3001'
   }
 }
 
 module.exports = nextConfig
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   test: /\\.(png|jp(e*)g|svg|gif)$/, 
-//   use: ['file-loader'],
-// }
-
-
-
-// module.exports = nextConfig
--- pages/api/loggin.ts
+++ pages/api/loggin.ts
@@ -1,20 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 const cookie = require('cookie');
+const cors = require('cors');
 
 
 export default ( req : NextApiRequest, res : NextApiResponse) => {
-    res.setHeader(
-        "Set-Cookie",
-        cookie.serialize(
-            "token", req.body.cookieToken,
-            {
-                httpOnly : true,
-                sameSite: 'none', 
-                secure : true,
-                // secure : process.env.NODE_ENV !== "development",
-                maxAge: 60 * 60 * 12, // 12 hora
-                path: "/"
-            }) 
-    )
-    res.status(200).json({success : true})
+    cors({ origin: '*' })(req, res, () => {
+        res.setHeader(
+            "Set-Cookie",
+            cookie.serialize(
+                "token", req.body.cookieToken,
+                {
+                    httpOnly : true,
+                    sameSite: 'none', 
+                    secure : true,
+                    // secure : process.env.NODE_ENV !== "development",
+                    maxAge: 60 * 60 * 12, // 12 hora
+                    path: "/"
+                }) 
+        )
+        res.status(200).json({success : true})
+      });
 }
