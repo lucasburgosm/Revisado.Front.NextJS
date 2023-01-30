bbafb4316b6e9c4411c4a3e7d7bb5f07482dedf1
Joaquin Montesano <montesano1994@gmail.com>
2023-01-29 23:46:26 -0300

    added loading when switch page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,46 @@ import '../styles/globals.css'
 import '../styles/footer.css'
 import type { AppProps } from 'next/app'
 import Layout from '../src/components/Layout'
+import LayoutShop from '../src/components/Layout_shop'
 import UseContextLogin from '../src/Hooks/UseContextLogin'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import Router from "next/router";
 
 
-function MyApp({ Component, pageProps, ...AppProps }: AppProps) {
+export default function MyApp({ Component, pageProps, ...AppProps }: AppProps) {
     const [loading, setLoading] = useState(false)
-    Router.events.on("routeChangeStart", (url) => { setLoading(true) });
-    Router.events.on("routeChangeComplete", (url) => { setLoading(false) });
+    const [targetRoute, setTargetRoute] = useState("");
+    Router.events.on("routeChangeStart", (url) => {
+        setLoading(true);
+        setTargetRoute(url);
+    });
+    Router.events.on("routeChangeComplete", () => {
+        setLoading(false);
+    });
 
     const getContent = () => {
+
         if ([`/nosotros`].includes(AppProps.router.pathname)) { return (<Component {...pageProps} />) }
-        return (
+        if (targetRoute === "/shop") {
+            return (
+                <Layout>
+                    {loading ?
+                    <>
+                        <h1 style={{padding:'50px 0',textAlign:'center'}}> Shop is loading, be patient... <br></br>it may take from 1 second to 5 years....<br></br> The host where the server is hosted is very sloooow...</h1>
+                        <LayoutShop></LayoutShop>
+                    </>
+
+                        :
+                        <Component {...pageProps} />}
+                </Layout>
+
+            )
+        }
+        else return (
             <Layout>
                 {loading ?
-                    <p> is loading</p>
+                    <p>loading...</p>
                     :
                     <Component {...pageProps} />}
             </Layout>
@@ -38,7 +61,3 @@ function MyApp({ Component, pageProps, ...AppProps }: AppProps) {
         </div>
     )
 }
-
-
-export default MyApp
-
